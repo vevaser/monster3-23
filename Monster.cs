96cd927ab0ca3472a6ba93e2c8a756bf830f040d
Veniamin Sergeev <get-prog@mail.ru>
2023-03-04 12:36:34 +0500

    Adding class Monster

---
+++ Monster.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace monster3_23
+{
+    class Monster
+    {
+        public Monster()    // Конструктор 1
+        {
+            this.name = "Noname";
+            this.health = 100;
+            this.ammo = 100;
+        }
+
+        public Monster(string name) : this()  // Конструктор 2
+        {
+            this.name = name;
+        }
+
+        public Monster(int health, int ammo, string name)  // Конструктор 3
+        {
+            this.name = name;
+            this.health = health;
+            this.ammo = ammo;
+        }
+
+        public int Health // свойство Health связано с полем health
+        {
+            get
+            {
+                return health;
+            }
+            set
+            {
+                if (value > 0) health = value;
+                else health = 0;
+            }
+
+        }
+
+        public int Ammo // свойство Ammo связано с полем ammo
+        {
+            get
+            {
+                return ammo;
+            }
+            set
+            {
+                if (value > 0) ammo = value;
+                else ammo = 0;
+            }
+
+        }
+
+        public string Name // свойство Name связано с полем Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+
+        }
+
+        public void Passport()
+        {
+            Console.WriteLine("Monster {0} \t healh={1} ammo={2}", name, health, ammo);
+        }
+
+        private string name;
+        private int health;
+        private int ammo;
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,6 +10,20 @@ namespace monster3_23
     {
         static void Main(string[] args)
         {
+
+            Monster Masha = new Monster(200, 200, "Masha");
+            Masha.Passport();
+            --Masha.Health;
+            Masha.Ammo += 100;
+            Masha.Passport();
+
+            Monster Vasya = new Monster();
+            Vasya.Passport();
+            Vasya.Name = "Vasya";
+            Vasya.Passport();
+
+
+            Console.ReadKey();
         }
     }
 }
